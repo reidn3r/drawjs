efc87af88bab8392dff1951bdab3202533468648
reidn3r <reidnersteam@hotmail.com>
2023-02-25 23:11:35 -0300

    rgb tensor fixed

--- controllers/draw.js
+++ controllers/draw.js
@@ -25,7 +25,10 @@ const drawPage = async(req, res) => {
         const model = await tf.loadLayersModel('file://tf_model/model.json');
 
         socket.on('url-emitter', (data) => {
-            let pixels = [];
+            // let pixels = [];
+            let rpixels = [];
+            let gpixels = [];
+            let bpixels = [];
             const buffer = Buffer.from(data.url, "base64");
             Jimp.read(buffer, (err, img) => {
                 if(err) console.log(err);
@@ -33,27 +36,43 @@ const drawPage = async(req, res) => {
                 // .write('drawjs.png')
                 .scan(0, 0, img.bitmap.width, img.bitmap.height, (x, y, i) => {
                     let r = img.bitmap.data[i]/255.0;
-                    pixels.push(r);
+                    rpixels.push(r);
                     
                     let g = img.bitmap.data[i + 1]/255.0;
-                    pixels.push(g);
+                    gpixels.push(g);
                     
                     let b = img.bitmap.data[i + 2]/255.0;
-                    pixels.push(b);
+                    bpixels.push(b);
                     
                 })
-                let tensor = tf.tensor3d(pixels,[64,64,3], 'float32').expandDims(0);
+                let rtensor = tf.tensor1d(rpixels, 'float32');
+                let gtensor = tf.tensor1d(gpixels, 'float32');
+                let btensor = tf.tensor1d(bpixels, 'float32');
+
+                let tensor = rtensor.concat(gtensor).concat(btensor).reshape([64, 64, 3]).expandDims(0);
+
+                // let tensor = tf.tensor3d(pixels,[64,64,3], 'float32').expandDims(0);
+                // tensor.print();
                 let output = model.predict(tensor).argMax(1);
                 let prob = model.predict(tensor).arraySync()[0][output.arraySync([0])];
                 
-                prob < 0.5 ? console.log("indefinido") : console.log('out:', classes[output.arraySync([0])]);
+                prob < 0.1 ? console.log("indefinido") : console.log('out:', classes[output.arraySync([0])]);
 
                 console.log("probabilidade: ", prob);
                 console.log('\n');
-
+                
                 //free memory
                 tf.dispose(tensor);
                 tf.dispose(output);
+                tf.dispose(prob);
+
+                tf.dispose(rtensor);
+                tf.dispose(gtensor);
+                tf.dispose(btensor);
+
+                rpixels = null;
+                gpixels = null;
+                bpixels = null;
             })
         }) 
     })
